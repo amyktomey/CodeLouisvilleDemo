529ac8391f1b78f0ea8b32c62048936d5c19419e
Amy Tomey <amy.tomey59@@gmail.com>
2022-01-24 22:30:50 -0500

    Homework + Extra Credit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Amy, and I can copunt to ten. Watch!!");
+
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+            }
+            Console.WriteLine("Ta-DAH!");
         }
     }
 }
