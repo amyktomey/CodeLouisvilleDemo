639a5cacf4c3e7c4a87672832e89d7ff5f993830
Amy Tomey <amy.tomey59@@gmail.com>
2022-02-13 10:53:00 -0500

    homework step one, create - copy/paste

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,14 @@
-﻿using CodeLouisvilleLibrary;
+﻿//using CodeLouisvilleLibrary:
 
 namespace CodeLouisvilleProjectBase;
-{
     class Program
         {
             static void Main(string[] args)
-            { 
-            bool understood  = CodeLouisvilleProjectBase.IsUnderstood;
+            {
+        bool doesThisMakeSense = CodeLouisvilleProjectBase.Prompt4YesNo("Does this make sense? ");
+                        if (doesThisMakeSense)
+                            Console.WriteLine("I've got this!!!");
+                        else
+                            Console.WriteLine("Blame John");
             }
-        }
-}
-
+     }
