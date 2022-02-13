4bb63b9b41d0e391ee7c5f4e98cc239008cfdd1f
Amy Tomey <amy.tomey59@@gmail.com>
2022-02-13 11:09:20 -0500

    corrected inconsistencies in namespaces and file names

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,11 @@
-﻿//using CodeLouisvilleLibrary:
+﻿using CodeLouisvilleLibrary;
 
 namespace CodeLouisvilleProjectBase;
     class Program
         {
             static void Main(string[] args)
             {
-        bool doesThisMakeSense = CodeLouisvilleProjectBase.Prompt4YesNo("Does this make sense? ");
+        bool doesThisMakeSense = CodeLouisvilleAppBase.Prompt4YesNo("Does this make sense? ");
                         if (doesThisMakeSense)
                             Console.WriteLine("I've got this!!!");
                         else
